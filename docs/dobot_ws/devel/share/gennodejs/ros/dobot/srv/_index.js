3e0899726b8b99bdc15e530d2d58f4a0815f11e7
guhd <guhd@shanghaitech.edu.cn>
2017-12-08 16:44:52 +0800

    update_on_2017.12.8_about_dobot_robotarm

---
+++ docs/dobot_ws/devel/share/gennodejs/ros/dobot/srv/_index.js
@@ -0,0 +1,120 @@
+
+"use strict";
+
+let GetJOGCoordinateParams = require('./GetJOGCoordinateParams.js')
+let SetPTPCmd = require('./SetPTPCmd.js')
+let SetPTPCoordinateParams = require('./SetPTPCoordinateParams.js')
+let SetWAITCmd = require('./SetWAITCmd.js')
+let SetCmdTimeout = require('./SetCmdTimeout.js')
+let GetIOADC = require('./GetIOADC.js')
+let GetARCParams = require('./GetARCParams.js')
+let SetARCParams = require('./SetARCParams.js')
+let SetQueuedCmdStopExec = require('./SetQueuedCmdStopExec.js')
+let GetPose = require('./GetPose.js')
+let GetDeviceName = require('./GetDeviceName.js')
+let SetJOGJointParams = require('./SetJOGJointParams.js')
+let ClearAllAlarmsState = require('./ClearAllAlarmsState.js')
+let GetIODO = require('./GetIODO.js')
+let GetCPCmd = require('./GetCPCmd.js')
+let GetEndEffectorGripper = require('./GetEndEffectorGripper.js')
+let GetJOGJointParams = require('./GetJOGJointParams.js')
+let SetQueuedCmdStartExec = require('./SetQueuedCmdStartExec.js')
+let GetHOMEParams = require('./GetHOMEParams.js')
+let SetJOGCommonParams = require('./SetJOGCommonParams.js')
+let GetPTPCoordinateParams = require('./GetPTPCoordinateParams.js')
+let SetIOPWM = require('./SetIOPWM.js')
+let SetHOMEParams = require('./SetHOMEParams.js')
+let SetHOMECmd = require('./SetHOMECmd.js')
+let GetJOGCommonParams = require('./GetJOGCommonParams.js')
+let SetQueuedCmdForceStopExec = require('./SetQueuedCmdForceStopExec.js')
+let SetEndEffectorLaser = require('./SetEndEffectorLaser.js')
+let GetEndEffectorParams = require('./GetEndEffectorParams.js')
+let SetPTPCommonParams = require('./SetPTPCommonParams.js')
+let SetTRIGCmd = require('./SetTRIGCmd.js')
+let GetPTPJointParams = require('./GetPTPJointParams.js')
+let GetDeviceVersion = require('./GetDeviceVersion.js')
+let SetIOMultiplexing = require('./SetIOMultiplexing.js')
+let SetPTPJumpParams = require('./SetPTPJumpParams.js')
+let GetIOPWM = require('./GetIOPWM.js')
+let SetQueuedCmdClear = require('./SetQueuedCmdClear.js')
+let GetDeviceSN = require('./GetDeviceSN.js')
+let SetEndEffectorGripper = require('./SetEndEffectorGripper.js')
+let SetCPCmd = require('./SetCPCmd.js')
+let SetDeviceName = require('./SetDeviceName.js')
+let GetPTPJumpParams = require('./GetPTPJumpParams.js')
+let GetPTPCommonParams = require('./GetPTPCommonParams.js')
+let SetEMotor = require('./SetEMotor.js')
+let SetJOGCoordinateParams = require('./SetJOGCoordinateParams.js')
+let SetCPParams = require('./SetCPParams.js')
+let GetEndEffectorLaser = require('./GetEndEffectorLaser.js')
+let SetJOGCmd = require('./SetJOGCmd.js')
+let GetIOMultiplexing = require('./GetIOMultiplexing.js')
+let SetEndEffectorParams = require('./SetEndEffectorParams.js')
+let SetEndEffectorSuctionCup = require('./SetEndEffectorSuctionCup.js')
+let SetPTPJointParams = require('./SetPTPJointParams.js')
+let GetIODI = require('./GetIODI.js')
+let GetEndEffectorSuctionCup = require('./GetEndEffectorSuctionCup.js')
+let GetAlarmsState = require('./GetAlarmsState.js')
+let GetCPParams = require('./GetCPParams.js')
+let SetIODO = require('./SetIODO.js')
+let SetARCCmd = require('./SetARCCmd.js')
+
+module.exports = {
+  GetJOGCoordinateParams: GetJOGCoordinateParams,
+  SetPTPCmd: SetPTPCmd,
+  SetPTPCoordinateParams: SetPTPCoordinateParams,
+  SetWAITCmd: SetWAITCmd,
+  SetCmdTimeout: SetCmdTimeout,
+  GetIOADC: GetIOADC,
+  GetARCParams: GetARCParams,
+  SetARCParams: SetARCParams,
+  SetQueuedCmdStopExec: SetQueuedCmdStopExec,
+  GetPose: GetPose,
+  GetDeviceName: GetDeviceName,
+  SetJOGJointParams: SetJOGJointParams,
+  ClearAllAlarmsState: ClearAllAlarmsState,
+  GetIODO: GetIODO,
+  GetCPCmd: GetCPCmd,
+  GetEndEffectorGripper: GetEndEffectorGripper,
+  GetJOGJointParams: GetJOGJointParams,
+  SetQueuedCmdStartExec: SetQueuedCmdStartExec,
+  GetHOMEParams: GetHOMEParams,
+  SetJOGCommonParams: SetJOGCommonParams,
+  GetPTPCoordinateParams: GetPTPCoordinateParams,
+  SetIOPWM: SetIOPWM,
+  SetHOMEParams: SetHOMEParams,
+  SetHOMECmd: SetHOMECmd,
+  GetJOGCommonParams: GetJOGCommonParams,
+  SetQueuedCmdForceStopExec: SetQueuedCmdForceStopExec,
+  SetEndEffectorLaser: SetEndEffectorLaser,
+  GetEndEffectorParams: GetEndEffectorParams,
+  SetPTPCommonParams: SetPTPCommonParams,
+  SetTRIGCmd: SetTRIGCmd,
+  GetPTPJointParams: GetPTPJointParams,
+  GetDeviceVersion: GetDeviceVersion,
+  SetIOMultiplexing: SetIOMultiplexing,
+  SetPTPJumpParams: SetPTPJumpParams,
+  GetIOPWM: GetIOPWM,
+  SetQueuedCmdClear: SetQueuedCmdClear,
+  GetDeviceSN: GetDeviceSN,
+  SetEndEffectorGripper: SetEndEffectorGripper,
+  SetCPCmd: SetCPCmd,
+  SetDeviceName: SetDeviceName,
+  GetPTPJumpParams: GetPTPJumpParams,
+  GetPTPCommonParams: GetPTPCommonParams,
+  SetEMotor: SetEMotor,
+  SetJOGCoordinateParams: SetJOGCoordinateParams,
+  SetCPParams: SetCPParams,
+  GetEndEffectorLaser: GetEndEffectorLaser,
+  SetJOGCmd: SetJOGCmd,
+  GetIOMultiplexing: GetIOMultiplexing,
+  SetEndEffectorParams: SetEndEffectorParams,
+  SetEndEffectorSuctionCup: SetEndEffectorSuctionCup,
+  SetPTPJointParams: SetPTPJointParams,
+  GetIODI: GetIODI,
+  GetEndEffectorSuctionCup: GetEndEffectorSuctionCup,
+  GetAlarmsState: GetAlarmsState,
+  GetCPParams: GetCPParams,
+  SetIODO: SetIODO,
+  SetARCCmd: SetARCCmd,
+};
